e943c973eec5aeb9bcaff3dfd088126a1befbf2f
youssef dardeer <youssef.dardeer.mousa@gmail.com>
2024-12-30 12:53:06 +0200

    new mail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,15 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
+
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'mail.alephyaa.net',
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      pass: process.env.EMAIL_PASS, // Keep password in environment variables for security
     },
   });
 
