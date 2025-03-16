336079d0d357b050ce8f93c4d046b1da14381366
schoolbookclubs <schoolbookclubs@gmail.com>
2025-03-16 03:54:07 +0200

    Update book.router.js

--- modules/Book/book.router.js
+++ modules/Book/book.router.js
@@ -21,6 +21,9 @@ router
         createBook
     );
 
+// Route to get books by school code without middleware
+router.post('/getBooksBySchoolCode', getBooksBySchoolCode);
+
 router
     .route('/:id')
     .put(
@@ -30,8 +33,7 @@ router
     )
     .delete(protect, deleteBook);
 
-// Route to get books by school code without middleware
-router.post('/getBooksBySchoolCode', express.json(), getBooksBySchoolCode);
+
 
 // Route to get books by teacher ID (no middleware)
 router.get('/teacher/:teacherId/books', getBooksByTeacherId);
