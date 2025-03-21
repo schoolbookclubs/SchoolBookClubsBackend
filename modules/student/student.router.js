b6329a2565bf3aa5866ee75b36d36ed2098dc53a
schoolbookclubs <schoolbookclubs@gmail.com>
2025-03-21 02:18:57 +0200

    Update student.router.js

--- modules/student/student.router.js
+++ modules/student/student.router.js
@@ -2,7 +2,7 @@ import express from 'express';
 import * as studentController from './student.controller.js';
 
 const router = express.Router();
-
+router.post('/change-password', authMiddleware, changePassword);
 router.post('/signup', studentController.signup);
 router.post('/login', studentController.login);
 router.post('/forget-password', studentController.forgetPassword);
